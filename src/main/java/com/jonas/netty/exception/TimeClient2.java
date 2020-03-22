7e6bfe7df3f8c6f04c8e112e92721cd1227dd638
shenjy <shenjy24@gmail.com>
2020-03-22 16:01:53 +0800

    feat: Netty模拟TCP粘包拆包问题

---
+++ src/main/java/com/jonas/netty/exception/TimeClient2.java
@@ -0,0 +1,49 @@
+package com.jonas.netty.exception;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * TimeClient
+ *
+ * @author shenjy
+ * @version 1.0
+ * @date 2020-03-20
+ */
+public class TimeClient2 {
+    public void connect(String host, int port) {
+        //配置客户端NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeClientHandler2());
+                        }
+                    });
+            //发起异步连接操作
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            //等待客户端链路关闭
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            //优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TimeClient2().connect("127.0.0.1", 8080);
+    }
+}
